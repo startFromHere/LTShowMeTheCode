e98b26417a9ca013cd9fec8210c56910cdfd96cf
lt80451 <liutao80451@offcn.com>
2021-03-15 18:15:44 +0800

    init

---
+++ LTLoadHookerLib/LTLoadHookerLib/LTLoadHookerLib/LTLoadHookerLib.h
@@ -0,0 +1,18 @@
+//
+//  LTLoadHookerLib.h
+//  LTLoadHookerLib
+//
+//  Created by 刘涛 on 2021/2/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LTLoadHookerLib.
+FOUNDATION_EXPORT double LTLoadHookerLibVersionNumber;
+
+//! Project version string for LTLoadHookerLib.
+FOUNDATION_EXPORT const unsigned char LTLoadHookerLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LTLoadHookerLib/PublicHeader.h>
+
+
---
+++ LTLoadHookerLib/LTLoadHookerLib/LTLoadHookerLib/LoadHooker.h
@@ -0,0 +1,18 @@
+//
+//  LoadHooker.h
+//  LTShowMeTheCode
+//
+//  Created by 刘涛 on 2021/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoadHooker : NSObject
+
++ (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
